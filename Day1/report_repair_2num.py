d728d4d89205e87f32fc31c46d59bcce887dd078
ejrossma <ejrossma@ucsc.edu>
2020-11-30 21:46:26 -0800

    Added some new folders

---
+++ Day1/report_repair_2num.py
@@ -0,0 +1,15 @@
+
+def report_repair(input_file):
+	file = open(input_file, "r")
+	loop = []
+	for x in file:
+		loop.append(int(x))
+	outcome = 0
+	for i in loop:
+		for j in loop:
+			num = i + j
+			if num == 2020:
+				outcome = i * j
+	print(outcome)
+
+report_repair("input.txt")
